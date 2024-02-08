629ce060b02d28e440f6e1e84096a1b0aff0b118
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2024-02-08 20:39:50 +0530

    code added

---
+++ File_Handling/CreatingNewFile.java
@@ -0,0 +1,18 @@
+package File_Handling;
+
+import java.io.*;
+
+public class CreatingNewFile {
+    public static void main(String[] args) {
+        File obj = new File("C:\\Users\\Vaibhav Mishra\\Desktop\\Java Backend\\File_Handling\\Bega.txt");
+        try {
+            if(obj.createNewFile()){
+                System.out.println("File created successfully..");
+            }else{
+                System.out.println("File already exist");
+            }
+        } catch (IOException e) {
+            System.out.println("Exception aagya h..");
+        }
+    }
+}
