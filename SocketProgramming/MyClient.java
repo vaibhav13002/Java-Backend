d147f63f02367772e34b7c3533f5c786753e21e3
VaishaliMishra111 <vaibhavmishra.bs@gmail.com>
2024-02-20 22:34:38 +0530

    code added

--- SocketProgramming/MyClient.java
+++ SocketProgramming/MyClient.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 public class MyClient {
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("localhost", 6666);  // kisi bhi server ko contact krne ke liye we nee {Ip Address and port number}, son send particular type of data to particular server
+            Socket s = new Socket("localhost", 6666);  // kisi bhi server ko contact krne ke liye we nee {Ip Address and port number}, son send particular type of data to particular server.
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             dout.writeUTF("Hello Server");
             dout.flush();
